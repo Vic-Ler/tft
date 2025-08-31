92efb9028b0852551a7ce58a450a13eb0ad16d7e
Victoria <vic.lercher@outlook.com>
2025-08-31 14:11:51 +0200

    Reformatted code using API fetch data function to reduce complexity

--- get_data.R
+++ get_data.R
@@ -7,7 +7,7 @@ library(cli) #for spinner alert info
 #parameters are defined in report_script and therefore commented here
 #FYI: anyone can get an API key by creating an account on RIOT's developer platform 
 
-#api_key = "RGAPI-465f712d-4b89-4445-8805-db3341774632"
+#api_key = Sys.getenv("RIOT_API")
 #summoner_name = "LivingWeapon" #official player name as displayed in-game
 #region = "europe" 
 #sub_region = "euw1"
@@ -22,95 +22,61 @@ library(cli) #for spinner alert info
 #/tft/match/v1/matches/{matchId}
 
 #################
+#API FUNCTION 
+fetch_data <- function(url, api_key) {
+  #returns API data based on input
+  response <- GET(url, 
+                  add_headers("X-Riot-Token" = api_key))
+  if (status_code(response) == 200) {
+    data <- fromJSON(content(response,
+                             "text", 
+                             encoding = "UTF-8"))
+    print("Data fetched successfully")
+    return(data)
+  } else {
+    print(paste("Error fetching data:", status_code(response)))
+  }}
+#################
 cli::cli_alert_info("Determining PUUID - Unique Player Key...")
 
-url_puuid <- paste0("https://", 
-                    region, 
-                    ".api.riotgames.com/riot/account/v1/accounts/by-riot-id/",
-                    summoner_name, "/", 
-                    tagline)
+summoner_data <- fetch_data(paste0("https://", 
+                                   region, 
+                                   ".api.riotgames.com/riot/account/v1/accounts/by-riot-id/",
+                                   summoner_name, "/", 
+                                   tagline),
+                            api_key)
 
-response_uuid <- GET(url_puuid, 
-                     add_headers("X-Riot-Token" = api_key))
-
-if (status_code(response_uuid) == 200) {
-  summoner_data <- fromJSON(content(response_uuid, 
-                                    "text", 
-                                    encoding = "UTF-8"))
-  puuid <- summoner_data$puuid
-  print(paste("PUUID obtained successfully:", puuid))
-} else {
-  print(paste("Error fetching data:", status_code(response_uuid)))
-}
+puuid <- summoner_data$puuid
 
 #################
 cli::cli_alert_info("Fetching TFT League Data...")
 
-url_league <- paste0("https://", sub_region,
-                     ".api.riotgames.com/tft/league/v1/by-puuid/", 
-                     puuid)
-
-response_league <- GET(url_league, 
-                       add_headers("X-Riot-Token" = api_key))
-
-if (status_code(response_league) == 200) {
-  tft_overview_data <- fromJSON(content(response_league, 
-                                        "text", 
-                                        encoding = "UTF-8"))
-  print("TFT Overview data obtained successfully")
-} else {
-  print(paste("Error fetching data:", status_code(response_league)))
-}
+tft_overview_data <- fetch_data(paste0("https://", sub_region,
+                                       ".api.riotgames.com/tft/league/v1/by-puuid/", 
+                                       puuid),
+                                api_key)
 #################
 cli::cli_alert_info("Fetching TFT Match IDs...")
 
-url_match <- paste0("https://", 
-                    region, 
-                    ".api.riotgames.com/tft/match/v1/matches/by-puuid/", 
-                    puuid, 
-                    "/ids")
-
-response_match <- GET(url_match, 
-                      add_headers("X-Riot-Token" = api_key))
-
-if (status_code(response_match) == 200) {
-  match_ids <- fromJSON(content(response_match, 
-                                        "text", 
-                                        encoding = "UTF-8"))
-  print("TFT Match IDs obtained successfully")
-} else {
-  print(paste("Error fetching data:", status_code(response_match)))
-}
+match_ids <- fetch_data(paste0("https://", 
+                               region, 
+                               ".api.riotgames.com/tft/match/v1/matches/by-puuid/", 
+                               puuid, 
+                               "/ids"),
+                        api_key)
 #################
 cli::cli_alert_info("Fetching TFT Match Details...")
 
-fetch_match_details <- function(match_id, puuid, region, api_key) {
-  #returns match details as a tibble based on puuid
-  url_match_details <- paste0("https://", 
-                region, 
-                ".api.riotgames.com/tft/match/v1/matches/", 
-                match_id)
-  
-  response_match_details <- GET(url_match_details, 
-                                add_headers("X-Riot-Token" = api_key))
-  
-  if (status_code(response_match_details) == 200) {
-    match_data_raw <- fromJSON(content(response_match_details, 
-                                       "text", 
-                                       encoding = "UTF-8")) 
-    
-    match_participants <- match_data_raw$info$participants[match_data_raw$info$participants$puuid == puuid, ] %>% as_tibble()
-    match_overview <- match_data_raw$info[c("game_datetime", "gameId", "game_length", "tft_game_type")] %>% as_tibble()
-    match_data <- cbind(match_overview, match_participants)
-    return(match_data)
-  } else {
-    print(paste("Error fetching data:", status_code(response_match_details)))
-    return(NULL)
-  }}
-
-#looping through match ids to extract match data
 match_details <- {} %>% as_tibble()
+  
 for (i in match_ids) {
-  match_data <- fetch_match_details(i, puuid, region, api_key)
+  match_data_raw <- fetch_data(paste0("https://", 
+                                      region, 
+                                      ".api.riotgames.com/tft/match/v1/matches/", 
+                                      i),
+                               api_key)
+  match_participants <- match_data_raw$info$participants[match_data_raw$info$participants$puuid == puuid, ] %>% as_tibble()
+  match_overview <- match_data_raw$info[c("game_datetime", "gameId", "game_length", "tft_game_type")] %>% as_tibble()
+  match_data <- cbind(match_overview, match_participants)
   match_details <- bind_rows(match_details, match_data)
 }
